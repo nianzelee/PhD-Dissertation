f0434a3a6d679133a03bef2a2391b1f184d7f7d2
nianze <nianzelee@gmail.com>
2021-04-13 10:11:45 +0800

    Add quantile plots for ER-SSAT
    - Copy new ER-SSAT results to its chapter folder
    - Delete the old results and csv files
    - Update the tex files of quantile plots

--- paper/exist-random-ssat/evaluation/process-data.sh
+++ paper/exist-random-ssat/evaluation/process-data.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-timestamp='erssat-full.2021-01-29_16-19-54'
-compared_approaches=('bare-BDD'
-                     'default-BDD')
+timestamp='erssat.2021-04-09_12-40-52'
+compared_approaches=('bare-BDD' 'default-BDD')
+formula_families=('Random' 'Application')
+column_identifiers=('cputime' 'memory')
 
 echo "Generating CSV files of erssat for quantile plots ..."
 for approach in "${compared_approaches[@]}"; do
-    echo "  > Configuration: ${approach}"
-    ./quantile-generator.py "./results/${timestamp}.results.${approach}.ExistRandomExist.xml.bz2" \
-        > "./csv/erssat.${approach}.quantile.csv"
+    for family in "${formula_families[@]}"; do
+        for identifier in "${column_identifiers[@]}"; do
+            echo "  > Configuration: ${approach}; Family: ${family}; Identifier: ${identifier}"
+            ./quantile-generator.py --sort-by "${identifier}" \
+                "./results/${timestamp}.results.${approach}.${family}.xml.bz2" \
+                > "./csv/erssat.${approach}.${family}.quantile.${identifier}.csv"
+        done
+    done
 done
 
-timestamp='dcssat-full.2021-01-29_19-06-55'
+timestamp='dcssat-er.2021-04-09_23-47-31'
 compared_approaches=('default')
 
 echo "Generating CSV files of dcssat for quantile plots ..."
 for approach in "${compared_approaches[@]}"; do
-    echo "  > Configuration: ${approach}"
-    ./quantile-generator.py "./results/${timestamp}.results.${approach}.ExistRandomExist.xml.bz2" \
-        > "./csv/dcssat.${approach}.quantile.csv"
+    for family in "${formula_families[@]}"; do
+        for identifier in "${column_identifiers[@]}"; do
+            echo "  > Configuration: ${approach}; Family: ${family}; Identifier: ${identifier}"
+            ./quantile-generator.py --sort-by "${identifier}" \
+                "./results/${timestamp}.results.${approach}.${family}.xml.bz2" \
+                > "./csv/dcssat.${approach}.${family}.quantile.${identifier}.csv"
+        done
+    done
 done
