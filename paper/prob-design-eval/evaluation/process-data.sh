d651edd5778cf241a57a6cace97a8aa7c088c969
nianze <nianzelee@gmail.com>
2021-04-22 11:57:40 +0800

    Create PEC and MPEC tables
    - Write a script to convert *.xml.bz2 files to *.csv files used by pgfplotstable
    - Remove rows that no tool can solve

--- paper/prob-design-eval/evaluation/process-data.sh
+++ paper/prob-design-eval/evaluation/process-data.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-table-generator -x PEC.xml -f csv -o ./csv --no-diff
-./csv_parser.py
+defect_rates=('D-0.01' 'D-0.10')
+
+echo "Generating CSV files for PEC tables ..."
+for defect in "${defect_rates[@]}"; do
+    table-generator -x PEC-"${defect}".xml -f csv -o ./csv --no-diff
+    ./PEC_csv_parser.py "${defect}"
+done
+
+echo "Generating CSV files for MPEC tables ..."
+for defect in "${defect_rates[@]}"; do
+    table-generator -x MPEC-"${defect}".xml -f csv -o ./csv --no-diff
+    ./MPEC_csv_parser.py "${defect}"
+done
